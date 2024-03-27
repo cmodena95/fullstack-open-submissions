f6222730bd0ef6c5131699fa19c316cbed1b33fe
Camila Modena <itscamilla@hotmail.com>
2024-03-27 09:28:21 +0100

    course info step 8

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,57 @@
+const Header = ({ course }) => <h1>{course}</h1>
+
+const Total = ({ sum }) => <p>Number of exercises {sum}</p>
+
+const Part = ({ part }) => 
+  <p>
+    {part.name} {part.exercises}
+  </p>
+
+const Content = ({ parts }) => 
+  <>
+    {parts.map(part => <Part key={part.id} part={part}/>)}   
+  </>
+
+const Course = ({course}) => {
+  const total = course.parts.reduce((s, p) => {
+      return s + p.exercises
+    }, 0
+  )
+
+  return (
+    <div>
+      <Header course={course.name}/>
+      <Content parts={course.parts}/>
+      <Total sum={total} />
+    </div>
+  )
+}
+
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
