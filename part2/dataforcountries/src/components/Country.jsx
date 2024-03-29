6ccb619aa9157ac767732a111448b3b9a2686df9
Camila Modena <itscamilla@hotmail.com>
2024-03-29 10:35:16 +0100

    languages

--- part2/dataforcountries/src/components/Country.jsx
+++ part2/dataforcountries/src/components/Country.jsx
@@ -3,11 +3,17 @@ const Country = ({ country }) => {
         return null
     }
 
+
     return (
         <div>
             <h1>{country.name.common}</h1>
             <p>Capital {country.capital[0]}</p>
             <p>Area {country.area}</p>
+
+            <h2>Languages</h2>
+            <ul>
+                {Object.values(country.languages).map((lang) => <li key={lang}>{lang}</li>)}
+            </ul>
         </div>
     )
 }
